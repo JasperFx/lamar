57fc7d7f56d61d5e83eb6fa63254c0330b94841e
Arish Labroo <arishl@zillowgroup.com>
2018-09-11 20:23:57 -0700

    add overload to prevent adding default policies for IOptions<> and ILogger<>

--- src/Lamar.Microsoft.DependencyInjection/WebHostBuilderExtensions.cs
+++ src/Lamar.Microsoft.DependencyInjection/WebHostBuilderExtensions.cs
@@ -8,32 +8,54 @@ namespace Lamar.Microsoft.DependencyInjection
     {
         public static IWebHostBuilder UseLamar(this IWebHostBuilder builder)
         {
-            return UseLamar(builder, registry: null);
+            return UseLamar(builder, registry: null, addDefaultPolicies: true);
+        }
+
+        public static IWebHostBuilder UseLamar(this IWebHostBuilder builder, bool addDefaultPolicies)
+        {
+            return UseLamar(builder, registry: null, addDefaultPolicies: addDefaultPolicies);
         }
 
         public static IWebHostBuilder UseLamar(this IWebHostBuilder builder, ServiceRegistry registry)
         {
-            return builder.ConfigureServices(services => { services.AddLamar(registry); });
-        }
-        
-        public static IServiceCollection AddLamar(this IServiceCollection services)
-        {
-            return AddLamar(services, registry: null);
+            return UseLamar(builder, registry: registry, addDefaultPolicies: true);
         }
 
-        public static IServiceCollection AddLamar(this IServiceCollection services, 
-            ServiceRegistry registry)
-        
+        public static IWebHostBuilder UseLamar(this IWebHostBuilder builder, ServiceRegistry registry, bool addDefaultPolicies)
+        {
+            return builder.ConfigureServices(services => { services.AddLamar(registry, addDefaultPolicies); });
+        }
+
+        public static IServiceCollection AddLamar(this IServiceCollection services)
+        {
+            return AddLamar(services, registry: null, addDefaultPolicies: true);
+        }
+
+        public static IServiceCollection AddLamar(this IServiceCollection services, bool addDefaultPolicies)
+        {
+            return AddLamar(services, registry: null, addDefaultPolicies: addDefaultPolicies);
+        }
+
+        public static IServiceCollection AddLamar(this IServiceCollection services, ServiceRegistry registry)
+        {
+            return AddLamar(services, registry: registry, addDefaultPolicies: true);
+        }
+
+        public static IServiceCollection AddLamar(this IServiceCollection services, ServiceRegistry registry, bool addDefaultPolicies)
+
         {
             services.AddSingleton<IServiceProviderFactory<ServiceRegistry>, LamarServiceProviderFactory>();
             services.AddSingleton<IServiceProviderFactory<IServiceCollection>, LamarServiceProviderFactory>();
 
-            services.AddSingleton<IRegistrationPolicy>(new LoggerPolicy());
-            services.AddSingleton<IFamilyPolicy>(new LoggerPolicy());
-            
-            services.AddSingleton<IRegistrationPolicy>(new OptionsPolicy());
-            services.AddSingleton<IFamilyPolicy>(new OptionsPolicy());
-            
+            if (addDefaultPolicies)
+            {
+                services.AddSingleton<IRegistrationPolicy>(new LoggerPolicy());
+                services.AddSingleton<IFamilyPolicy>(new LoggerPolicy());
+
+                services.AddSingleton<IRegistrationPolicy>(new OptionsPolicy());
+                services.AddSingleton<IFamilyPolicy>(new OptionsPolicy());
+            }
+
             if (registry != null)
             {
                 foreach (var descriptor in registry)
@@ -41,7 +63,7 @@ namespace Lamar.Microsoft.DependencyInjection
                     services.Add(descriptor);
                 }
             }
-            
+
             return services;
         }
     }
