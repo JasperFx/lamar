ac8183f8746f51ce66119b5a61eefecd4ee3aae5
andy <andydavies2020@gmail.com>
2018-10-27 11:03:41 +0300

    diagnostics: add aspnet core example

--- src/Lamar.AspNetCoreTests/Samples/StartUp.cs
+++ src/Lamar.AspNetCoreTests/Samples/StartUp.cs
@@ -1,7 +1,9 @@
-﻿using Lamar.Microsoft.DependencyInjection;
+﻿using System;
+using Lamar.Microsoft.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Lamar.Testing.AspNetCoreIntegration.Samples
 {
@@ -53,6 +55,42 @@ namespace Lamar.Testing.AspNetCoreIntegration.Samples
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseMvc();
+        }
+    }
+    // ENDSAMPLE
+
+    // SAMPLE: whatdoihave-aspnetcore
+    public class StartupWithDiagnostics
+    {
+        // Take in Lamar's ServiceRegistry instead of IServiceCollection
+        // as your argument, but fear not, it implements IServiceCollection
+        // as well
+        public void ConfigureContainer(ServiceRegistry services)
+        {
+            // Supports ASP.Net Core DI abstractions
+            services.AddMvc();
+            services.AddLogging();
+
+            // Also exposes Lamar specific registrations
+            // and functionality
+            services.Scan(s =>
+            {
+                s.TheCallingAssembly();
+                s.WithDefaultConventions();
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if(env.IsDevelopment())
+            {
+                var container = (IContainer)app.ApplicationServices;
+                // or write to your own Logger
+                Console.WriteLine(container.WhatDidIScan());
+                Console.WriteLine(container.WhatDoIHave());
+            }
+
             app.UseMvc();
         }
     }
