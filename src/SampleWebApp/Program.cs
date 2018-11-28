9677a8147c4ff9e2d7d4f1f9fef7ff8cea567a47
jeremydmiller <jeremydmiller@yahoo.com>
2018-11-28 14:47:01 -0600

    sample project to try to bring out Nuget problems

---
+++ src/SampleWebApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Lamar.Microsoft.DependencyInjection;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = new WebHostBuilder();
+            builder
+                // Replaces the built in DI container
+                // with Lamar
+                .UseLamar()
+
+                // Normal ASP.Net Core bootstrapping
+                .UseUrls("http://localhost:5002")
+                .UseKestrel()
+                .UseStartup<Startup>();
+
+            var host = builder.Build();
+            
+            host.Run();
+
+        }
+    }
+}
---
+++ src/SampleWebApp/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lamar;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApplication4
+{
+    public class Startup
+    {
+        // Take in Lamar's ServiceRegistry instead of IServiceCollection
+        // as your argument, but fear not, it implements IServiceCollection
+        // as well
+        public void ConfigureContainer(ServiceRegistry services)
+        {
+            // Supports ASP.Net Core DI abstractions
+            services.AddMvc();
+            services.AddLogging();
+
+            // Also exposes Lamar specific registrations
+            // and functionality
+            //services.Scan(s =>
+            //{
+            //    s.TheCallingAssembly();
+            //    s.WithDefaultConventions();
+            //});
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
