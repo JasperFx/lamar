b03d48c312b3c302a2d1e6523f553910b327c2a3
Donald Hamm <hammdo@live.com>
2019-01-30 15:32:24 -0600

    Test that shows the bug # 124 compilation error.

---
+++ Widget.Instance/BugWidget.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Widget.Core.Interfaces;
+
+namespace Widget.Instance
+{
+    public class BugWidget : IBugWidget
+    {
+        public bool IFixedWidget()
+        {
+            Console.WriteLine("I Fixed the Widget");
+            return true;
+        }
+    }
+}
---
+++ Widget.Registration/BugWidgetRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Lamar;
+using Widget.Core.Interfaces;
+using Widget.Instance;
+
+namespace Widget.Registration
+{
+    public class BugWidgetRegistration : ServiceRegistry
+    {
+        public BugWidgetRegistration()
+        {
+            For<IBugWidget>().Use<BugWidget>();
+        }
+
+    }
+}
