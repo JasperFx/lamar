709742c211bd91aa4de8ad2252f3669eadcc21d6
Mark Lopez <m@silvenga.com>
2019-06-17 20:46:43 -0500

    Created test case.

---
+++ src/Lamar.Testing/Bugs/Bug_167_implementation_factory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using StructureMap.Testing.Widget;
+using Xunit;
+
+namespace Lamar.Testing.Bugs
+{
+    public class Test
+    {
+        [Fact]
+        public void Should_resolve_ctor_injection()
+        {
+            var container = new Container(x =>
+            {
+                var serviceDescriptor = ServiceDescriptor.Transient(typeof(IWidget), provider => provider.GetRequiredService(typeof(AWidget)));
+                x.Add(serviceDescriptor);
+            });
+
+            var result = container.GetInstance<MyClass>();
+
+            result.ShouldBeOfType<MyClass>();
+        }
+
+        private class MyClass
+        {
+            public MyClass(IWidget widget)
+            {
+            }
+        }
+    }
+}
