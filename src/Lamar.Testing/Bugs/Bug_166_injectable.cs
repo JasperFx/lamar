41408d5c4a7bfca43e3ece23f9ff3fc30aa7f9d5
jeremydmiller <jeremydmiller@yahoo.com>
2019-07-03 15:10:43 -0500

    new test proving Injectable works. Closes GH-166

---
+++ src/Lamar.Testing/Bugs/Bug_166_injectable.cs
@@ -0,0 +1,39 @@
+using Shouldly;
+using Xunit;
+
+namespace Lamar.Testing.Bugs
+{
+    public class Bug_166_injectable
+    {
+        public interface IExecutionContext{}
+        public class ExecutionContext1 : IExecutionContext{}
+        public class ExecutionContext2 : IExecutionContext{}
+        
+        [Fact]
+        public void Lamar_Should_Ok()
+        {
+            var container = new Container(_ =>
+            {
+                // This is the ONLY registration you need
+                _.Injectable<IExecutionContext>();
+            });
+            
+            // If you need a value in the root container
+            container.Inject<IExecutionContext>(new ExecutionContext1());
+
+            container.GetInstance<IExecutionContext>()
+                .ShouldBeOfType<ExecutionContext1>();
+
+            
+            var nested = container.GetNestedContainer();
+
+            // Gotta do this too
+            var nestedContext = new ExecutionContext2();
+            nested.Inject(typeof(IExecutionContext), nestedContext, true);
+
+            
+            nested.GetInstance<IExecutionContext>()
+                .ShouldBeSameAs(nestedContext);
+        }
+    }
+}
