48f621a47d5b278646dee0d48f6d4fd06ca231a3
Aaron Burch <aaron@allbrightdesigns.com>
2020-04-22 12:56:53 -0700

    A quick implementation of activation interceptors. Would love some feedback.

---
+++ src/Lamar/IActivationInterceptor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lamar.IoC;
+
+namespace Lamar
+{
+    public interface IActivationInterceptor<T>
+    {
+        T Intercept(Type serviceType, T instance, Scope scope);
+    }
+}
