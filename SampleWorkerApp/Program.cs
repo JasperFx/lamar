cda52e2c6a57680a2950a654e68fb1f09d9a80b6
jeremydmiller <jeremydmiller@yahoo.com>
2020-07-18 14:40:33 -0500

    New return frame.

---
+++ SampleWorkerApp/Program.cs
@@ -0,0 +1,38 @@
+using System;
+using Lamar.Microsoft.DependencyInjection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SampleWorkerApp
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
+
+		// SAMPLE: startup-worker-service
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.UseLamar()
+				.ConfigureServices((hostContext, services) =>
+				{
+					services.AddHostedService<Worker>();
+				})
+				.ConfigureContainer<Lamar.ServiceRegistry>((context, services) =>
+				{
+					// Also exposes Lamar specific registrations
+					// and functionality
+					services.Scan(s =>
+					{
+						s.TheCallingAssembly();
+						s.WithDefaultConventions();
+					});
+				});
+		// ENDSAMPLE
+	}
+}
---
+++ src/LamarCompiler.Testing/Codegen/ReturnFrameTests.cs
@@ -0,0 +1,46 @@
+using System.Linq;
+using LamarCodeGeneration.Frames;
+using LamarCompiler.Scenarios;
+using Shouldly;
+using Xunit;
+
+namespace LamarCompiler.Testing.Codegen
+{
+    public class ReturnFrameTests
+    {
+        [Fact]
+        public void simple_use_case_no_value()
+        {
+            var result = CodegenScenario.ForBaseOf<ISimpleAction>(m => m.Frames.Add(new ReturnFrame()));
+            
+            result.LinesOfCode.ShouldContain("return;");
+        }
+        
+        [Fact]
+        public void return_a_variable_by_type()
+        {
+            var result = CodegenScenario.ForBuilds<int, int>(m => m.Frames.Return(typeof(int)));
+            
+            result.LinesOfCode.ShouldContain("return arg1;");
+            result.Object.Create(5).ShouldBe(5);
+        }
+        
+        [Fact]
+        public void return_explicit_variable()
+        {
+            var result = CodegenScenario.ForBuilds<int, int>(m =>
+            {
+                var arg = m.Arguments.Single();
+                m.Frames.Return(arg);
+            });
+            
+            result.LinesOfCode.ShouldContain("return arg1;");
+            result.Object.Create(5).ShouldBe(5);
+        }
+    }
+
+    public interface ISimpleAction
+    {
+        void Go();
+    }
+}
