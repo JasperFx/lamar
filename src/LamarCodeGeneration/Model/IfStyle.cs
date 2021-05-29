79a9e4311c43ca3b9ff8a3007adba355f3a695d2
Jeremy D. Miller <jeremydmiller@yahoo.com>
2021-05-29 13:36:21 -0500

    Added IfStyle for nested code generation

---
+++ src/LamarCodeGeneration/Model/IfStyle.cs
@@ -0,0 +1,44 @@
+﻿using LamarCodeGeneration.Util;
+
+namespace LamarCodeGeneration.Model
+{
+    public class IfStyle
+    {
+        private readonly bool _writes;
+        internal static readonly IfStyle If = new IfStyle("if");
+        internal static readonly IfStyle ElseIf = new IfStyle("else if");
+        internal static readonly IfStyle Else = new IfStyle("else");
+        internal static readonly IfStyle None = new IfStyle("else", false);
+
+        public string Code { get; }
+
+        private IfStyle(string code, bool writes = true)
+        {
+            _writes = writes;
+            Code = code;
+        }
+
+        public void Open(ISourceWriter writer, string condition)
+        {
+            if (_writes)
+            {
+                writer.Write(condition.IsEmpty()
+                    ? $"BLOCK:{Code}"
+                    : $"BLOCK:{Code} ({condition})");
+            }
+        }
+
+        public void Close(ISourceWriter writer)
+        {
+            if (_writes)
+            {
+                writer.FinishBlock();
+            }
+        }
+
+        public override string ToString()
+        {
+            return Code;
+        }
+    }
+}
