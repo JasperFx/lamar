9fb85804037e292adb140d199a39419cc67d6500
Jeremy D. Miller <jeremydmiller@yahoo.com>
2021-08-18 08:41:41 -0500

    Documentation Update for 5.0.0

---
+++ assets/guide_ioc_blazor.md.4b724a79.lean.js
@@ -0,0 +1,1 @@
+import{o as n,c as s,b as a}from"./app.80913d4a.js";const t='{"title":"Integration with Blazor","description":"","frontmatter":{},"relativePath":"guide/ioc/blazor.md","lastUpdated":1629294083756}',p={},o=a('',5);p.render=function(a,t,p,e,c,l){return n(),s("div",null,[o])};export{t as __pageData,p as default};
