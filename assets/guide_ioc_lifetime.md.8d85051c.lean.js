b9d91d03226015bf57561de488c3dcbbdd9f90bb
Jeremy D. Miller <jeremydmiller@yahoo.com>
2021-08-18 08:36:28 -0500

    Documentation Update for 5.0.0

---
+++ assets/guide_ioc_lifetime.md.8d85051c.lean.js
@@ -0,0 +1,1 @@
+import{o as n,c as s,a,b as t}from"./app.80913d4a.js";const e='{"title":"Service Lifetimes","description":"","frontmatter":{},"relativePath":"guide/ioc/lifetime.md","lastUpdated":1629293772480}',p={},o=t('',6),c=t('',3);p.render=function(t,e,p,i,l,u){return n(),s("div",null,[o,a(" snippet: sample_LifetimeRegistry "),c])};export{e as __pageData,p as default};
