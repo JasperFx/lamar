5487a5433ba4626dc215682e3fa2455d8aa70c96
mysticmind <babu.annamalai@gmail.com>
2021-08-24 21:23:05 +0530

    Documentation Update for 5.0.0

---
+++ assets/guide_ioc_resolving_index.md.5d3dc47d.lean.js
@@ -0,0 +1,1 @@
+import{o as e,c as r,b as i}from"./app.7c563280.js";const a='{"title":"Resolving Services","description":"","frontmatter":{},"relativePath":"guide/ioc/resolving/index.md","lastUpdated":1629820368417}',t={},o=i('',3);t.render=function(i,a,t,s,n,c){return e(),r("div",null,[o])};export{a as __pageData,t as default};
