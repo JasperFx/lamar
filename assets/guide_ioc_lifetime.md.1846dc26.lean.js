61c574ea58785e99485d0d5ae423811ec30f6f28
mysticmind <babu.annamalai@gmail.com>
2021-08-24 21:31:06 +0530

    Documentation Update for 5.0.0

---
+++ assets/guide_ioc_lifetime.md.1846dc26.lean.js
@@ -0,0 +1,1 @@
+import{o as n,c as s,a,b as t}from"./app.21b67795.js";const e='{"title":"Service Lifetimes","description":"","frontmatter":{},"relativePath":"guide/ioc/lifetime.md","lastUpdated":1629820849235}',p={},o=t('',6),c=t('',3);p.render=function(t,e,p,i,l,u){return n(),s("div",null,[o,a(" snippet: sample_LifetimeRegistry "),c])};export{e as __pageData,p as default};
