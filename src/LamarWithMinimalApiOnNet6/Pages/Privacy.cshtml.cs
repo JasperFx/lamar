9dfd3beb75231ff62370db771fb138e90b280878
Stephan <stephan.steiger@fhnw.ch>
2021-11-25 17:53:00 +0100

    Added minimal API sample for .net 6

---
+++ src/LamarWithMinimalApiOnNet6/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿namespace LamarWithMinimalApiOnNet6.Pages
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
