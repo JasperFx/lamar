1a2758ee9398f3e14a0156e8f76e140931f7e9c2
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2022-04-11 09:18:01 -0500

    bumping all the lamar code generation code to 6.1.2

--- src/Lamar.Diagnostics/ServiceCollectionExtensions.cs
+++ src/Lamar.Diagnostics/ServiceCollectionExtensions.cs
@@ -9,11 +9,13 @@ namespace Lamar.Diagnostics
         /// <summary>
         /// Adds an environment check to validate all the Lamar IoC configuration, including Lamar's own
         /// environment checks
+        /// <param name="mode">Specify the level of the environment checks. Default is Full</param>
         /// </summary>
-        public static void CheckLamarConfiguration(this IServiceCollection services)
+        public static void CheckLamarConfiguration(this IServiceCollection services, AssertMode mode = AssertMode.Full)
         {
-            services.CheckEnvironment("Lamar IoC Service Registrations",s => ((IContainer)s).AssertConfigurationIsValid(AssertMode.Full));
+            services.CheckEnvironment("Lamar IoC Service Registrations",s => ((IContainer)s).AssertConfigurationIsValid(mode));
             services.CheckEnvironment("Lamar IoC Type Scanning", s => TypeRepository.AssertNoTypeScanningFailures());
         }
     }
+
 }
