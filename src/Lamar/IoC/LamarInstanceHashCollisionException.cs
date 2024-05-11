258d5c24837a864651224ef8f9f5f0eb6898465a
Jordan Woodman <jordan.woodman@live.com>
2024-05-11 11:32:18 -0400

    Add optional hash collision mitigation to ServiceRegistry

---
+++ src/Lamar/IoC/LamarInstanceHashCollisionException.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using JasperFx.Core.Reflection;
+
+namespace Lamar.IoC;
+
+public class LamarInstanceHashCollisionException : LamarException
+{
+    public LamarInstanceHashCollisionException(int instanceHash, IEnumerable<Type> serviceTypes) : base(
+        $"Duplicate hash '{instanceHash}' generated for services: {string.Join(", ", serviceTypes.Select(x => x.FullNameInCode()))}")
+    {
+    }
+}
