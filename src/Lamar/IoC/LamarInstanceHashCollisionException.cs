574a89e53e1abe563560f52d4f3fc607f6e87ad9
Jordan Woodman <jordan.woodman@live.com>
2024-05-11 11:59:10 -0400

    add unit tests

--- src/Lamar/IoC/LamarInstanceHashCollisionException.cs
+++ src/Lamar/IoC/LamarInstanceHashCollisionException.cs
@@ -7,8 +7,13 @@ namespace Lamar.IoC;
 
 public class LamarInstanceHashCollisionException : LamarException
 {
+    public int InstanceHash { get; }
+    public IEnumerable<Type> ServiceTypes { get; }
+
     public LamarInstanceHashCollisionException(int instanceHash, IEnumerable<Type> serviceTypes) : base(
         $"Duplicate hash '{instanceHash}' generated for services: {string.Join(", ", serviceTypes.Select(x => x.FullNameInCode()))}")
     {
+        InstanceHash = instanceHash;
+        ServiceTypes = serviceTypes;
     }
 }
