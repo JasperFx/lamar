879b44a0a7cf244f0f19966511038cd22842e5e5
Jordan Woodman <jordan.woodman@buildium.com>
2024-12-06 13:15:52 -0500

    add new instance hash behavior

--- src/Lamar.Microsoft.DependencyInjection/LamarServiceProviderFactory.cs
+++ src/Lamar.Microsoft.DependencyInjection/LamarServiceProviderFactory.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using Lamar.IoC;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Lamar.Microsoft.DependencyInjection
 {
     public class LamarServiceProviderFactory : IServiceProviderFactory<ServiceRegistry>, IServiceProviderFactory<IServiceCollection>
     {
+        private readonly InstanceMapBehavior _instanceMapBehavior;
+        
+        public LamarServiceProviderFactory(InstanceMapBehavior instanceMapBehavior)
+        {
+            _instanceMapBehavior  = instanceMapBehavior;
+        }
+        
         public ServiceRegistry CreateBuilder(IServiceCollection services)
         {
             var registry = new ServiceRegistry();
@@ -15,12 +23,12 @@ namespace Lamar.Microsoft.DependencyInjection
 
         public IServiceProvider CreateServiceProvider(IServiceCollection containerBuilder)
         {
-            return new Container(containerBuilder);
+            return new Container(containerBuilder, _instanceMapBehavior);
         }
 
         public IServiceProvider CreateServiceProvider(ServiceRegistry containerBuilder)
         {
-            return new Container(containerBuilder);
+            return new Container(containerBuilder, _instanceMapBehavior);
         }
 
         IServiceCollection IServiceProviderFactory<IServiceCollection>.CreateBuilder(IServiceCollection services)
--- src/Lamar/IoC/Scope.cs
+++ src/Lamar/IoC/Scope.cs
@@ -31,10 +31,12 @@ public class Scope : IServiceContext, IServiceProviderIsService
     // don't build this if you don't need it
     private Dictionary<Type, object> _injected;
 
-    internal ImHashMap<InstanceIdentifier, object> Services = ImHashMap<InstanceIdentifier, object>.Empty;
+    internal InstanceMap Services;
 
-    public Scope(IServiceCollection services)
+    public Scope(IServiceCollection services, InstanceMapBehavior instanceMapBehavior = InstanceMapBehavior.Default)
     {
+        Services = new InstanceMap(instanceMapBehavior);
+
         Root = this;
 
         ServiceGraph = new ServiceGraph(services, this);
@@ -42,12 +44,14 @@ public class Scope : IServiceContext, IServiceProviderIsService
         ServiceGraph.Initialize();
     }
 
-    protected Scope()
+    protected Scope(InstanceMapBehavior instanceMapBehavior = InstanceMapBehavior.Default)
     {
+        Services = new InstanceMap(instanceMapBehavior);
     }
 
-    public Scope(ServiceGraph serviceGraph, Scope root)
+    public Scope(ServiceGraph serviceGraph, Scope root, InstanceMapBehavior instanceMapBehavior = InstanceMapBehavior.Default)
     {
+        Services = new InstanceMap(instanceMapBehavior);
         ServiceGraph = serviceGraph;
         Root = root ?? throw new ArgumentNullException(nameof(root));
     }
